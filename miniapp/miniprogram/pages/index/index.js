8ce8d5ced7f459ba3c271626e6d32364ea9734a2
saierti <310320717@qq.com>
2019-04-25 20:26:14 +0800

    小程序

---
+++ miniapp/miniprogram/pages/index/index.js
@@ -0,0 +1,44 @@
+const app = getApp();
+
+Page({
+  data:{
+    a:20,
+    b:false,
+    arr:[1,2,13],
+    selectedIndex:0,
+  },
+  fn(){
+    wx.getUserInfo({
+      success(res){
+        console.log(res);
+        alert("success")
+      },
+      fail(res){
+        console.log("失败")
+      }
+    })
+    // wx.showLoading({
+    //   title: 'waiting',
+    // })
+    console.log(this.a);
+  },
+  selectChange(e){
+    console.log(e);
+    this.setData({
+        selectedIndex:e.detail.value
+    })
+  },
+  //小程序启动后执行
+  onLaunch:function(){
+
+  },
+  //页面渲染后执行
+  onLoad:function(){
+    console.log(app,this.a)
+  },
+  a:12,
+  user:{
+    name:"greed"
+  }
+})
+
