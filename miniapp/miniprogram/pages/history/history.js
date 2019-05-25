7edebfb75370dec66cc4f7fbbe025c1a9aa1f094
Crazy492 <1056872781@qq.com>
2019-05-25 20:34:37 +0800

    history

--- miniapp/miniprogram/pages/history/history.js
+++ miniapp/miniprogram/pages/history/history.js
@@ -5,14 +5,40 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    activeID:0,
+    cards:[
+      {
+        dayStart:"4.25",
+        point:50,
+        dayEnd:"4.28"
+      },
+      {
+        dayStart:"4.22",
+        point:30,
+        dayEnd:"4.23"
+      },
+      {
+        dayStart:"5.25",
+        point:10,
+        dayEnd:"5.28"
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   currentChange(e){
+    this.setData({
+      activeID:e.detail.current
+    })
     console.log(e.detail.current)
+  },
+  clickCard(e){
+    console.log(e)
+    this.setData({
+      activeID:e.target.dataset.id
+    })
   },
   onLoad: function (options) {
 
